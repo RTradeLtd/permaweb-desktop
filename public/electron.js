829df6f2abad983e13543119cdb32c5733260deb
Shokunin <30949000+leshokunin@users.noreply.github.com>
2019-05-01 20:32:27 -0700

    Update electron.js

--- public/electron.js
+++ public/electron.js
@@ -10,7 +10,7 @@ let mainWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({ width: 900, height: 680 });
-  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  mainWindow.loadURL(isDev ? 'http://127.0.0.1:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   mainWindow.on('closed', () => mainWindow = null);
 }
 
