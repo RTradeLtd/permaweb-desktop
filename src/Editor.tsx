150ee76bfa3ed0152d10feeabef265fa618bf2d7
Lily <lily.bickerstaffe@live.co.uk>
2019-05-31 01:19:45 +0800

    Added emoji support and toggle markdown

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -4,29 +4,129 @@ import { inject } from "mobx-react";
 import Moment from 'react-moment';
 //@ts-ignore
 import Editor from "react-medium-editor";
+//@ts-ignore
+import MediumEditor from 'medium-editor'
 import Store from "./Store";
 // @ts-ignore
 import { Shortcuts } from 'react-shortcuts'
+import 'emoji-mart/css/emoji-mart.css'
+// @ts-ignore
+import { Emoji, Picker, emojiIndex } from 'emoji-mart'
+import { renderToString } from 'react-dom/server'
+// @ts-ignore
+import CodeMirror from 'react-codemirror'
+require('codemirror/lib/codemirror.css');
+// @ts-ignore
+import Showdown from 'showdown'
+// @ts-ignore
+import TurndownService from 'turndown'
 
 interface ArticleForm {
   store: Store;
 }
+
 @inject("store")
 class ArticleForm extends Component<ArticleForm> {
+  emojiRef?: HTMLElement;
+  editor: any;
+  markdownConverter: any;
+  htmlConverter: any;
   state = {
-    fileContent: ""
+    fileContent: "",
+    fileMarkdown: "",
+    showEmoji: false,
+    showMarkdown: false
   };
-  shouldComponentUpdate() {
-    return false
+  shouldComponentUpdate(_nextProps: any, nextState: any) {
+    return (this.state.showEmoji !== nextState.showEmoji) || (this.state.showMarkdown !== nextState.showMarkdown)
   };
+  componentDidMount() {
+    this.markdownConverter = new Showdown.Converter();
+    this.htmlConverter = new TurndownService({ headingStyle: 'atx' });
+
+    const el = document.getElementById("epona-editor");
+    this.editor = MediumEditor.getEditorFromElement(el);
+
+    const { file } = this.props.store;
+    const newContent = (file && file.stored.body) || ""
+    this.setState({ fileContent: newContent })
+    this.updateFileContent(newContent)
+    this.updateMarkdownFromContent(newContent)
+  }
   handleArticle = (e: FormEvent) => {
     e.preventDefault();
-    this.props.store.setFile(this.state.fileContent);
+    this.props.store.setFile(this.editor.getContent());
     this.props.store.createFile();
   };
-  updateContent = (content: string) => {
+  updateFileContent = (content: string) => {
+    this.setState({ fileContent: content, showEmoji: false })
+    this.editor.setContent(content);
+  }
+  updateMarkdownFromContent = (content: string) => {
+    let markdown = this.htmlConverter.turndown(content)
+    this.setState({ fileMarkdown: markdown })
+  }
+  checkFileContent = (content: string) => {
+    let matchColon = new RegExp(/[ >]:<\/[a-zA-Z0-9]+>$/)
+    let matchEmoji = new RegExp(/[ >:]:[a-zA-Z0-9\+_\-]+:/)
+    let emojiMatches = content.match(matchEmoji) || []
+
+    for (let index = 0; index < emojiMatches.length; index++) {
+      const emojiMatch = emojiMatches[index];
+      let searchText = emojiMatch.substring(2, emojiMatch.length - 1)
+      let colonText = `:${searchText}:`
+      let results = emojiIndex.search(searchText).filter((emoji: any) => {
+        return emoji.colons && emoji.colons === colonText
+      })
+
+      if (results && results.length > 0) {
+        let emoji = results[0]
+        let emojiHtml = renderToString(<span contentEditable={true} dangerouslySetInnerHTML={{ __html: Emoji({ html: true, emoji: emoji.colons, size: 24 }) }}></span>)
+
+        let replaceText = new RegExp(colonText, "g")
+        content = content.replace(replaceText, `${emojiHtml}.`)
+        this.updateFileContent(content)
+      }
+    }
+
+    if (matchColon.test(content)) {
+      this.setState({ showEmoji: true })
+    }
+
     this.setState({ fileContent: content })
+    this.updateMarkdownFromContent(content)
   };
+  toggleMarkdown = () => {
+    this.setState({ showMarkdown: !this.state.showMarkdown })
+  };
+  addEmoji = (emoji: any) => {
+    this.editor.pasteHTML(emoji.colons);
+    this.updateFileContent(this.editor.getContent())
+  };
+  emojiButton = () => {
+    const handleButtonClick = () => {
+      this.setState({ showEmoji: !this.state.showEmoji })
+    }
+    const EmojiButton = MediumEditor.Extension.extend({
+      name: 'emoji',
+      init: function () {
+        this.button = this.document.createElement('button');
+        this.button.classList.add('medium-editor-action');
+        this.button.innerHTML = ':)';
+        this.button.title = 'Emoji';
+        this.on(this.button, 'click', handleButtonClick);
+      },
+      getButton: function () {
+        return this.button;
+      }
+    });
+    return new EmojiButton()
+  }
+  updateMarkdownEvent = (markdown: any) => {
+    let html = this.markdownConverter.makeHtml(markdown);
+    this.updateFileContent(html);
+    this.setState({ fileMarkdown: markdown });
+  }
   clearFile = () => {
     this.props.store.clearFile();
   };
@@ -57,6 +157,14 @@ class ArticleForm extends Component<ArticleForm> {
               >
                 Back
               </Button>
+              <Button
+                onClick={() => {
+                  this.toggleMarkdown();
+                }}
+                primary
+              >
+                Toggle
+              </Button>
               <div style={{ float: "right" }}>
                 <Form onSubmit={this.handleArticle}>
                   <Form.Button content="Save" type={"submit"} />
@@ -69,21 +177,44 @@ class ArticleForm extends Component<ArticleForm> {
         <p>
           <Moment fromNow>{file && file.date}</Moment>
         </p>
-        <Editor
-          style={{
-            minHeight: "10vh",
-            fontSize: "16px",
-            border: "none",
-            overflow: "auto",
-            outline: "none",
-            WebkitBoxShadow: "none",
-            MozBoxShadow: "none",
-            BoxShadow: "none",
-            resize: "none"
-          }}
-          onChange={this.updateContent}
-          text={(file && file.stored.body) || ""}
-        />
+        {
+          this.state.showMarkdown ?
+            <CodeMirror
+              value={this.state.fileMarkdown}
+              onChange={this.updateMarkdownEvent}
+              options={{ lineNumbers: true }}
+            />
+            : <Editor
+              id="epona-editor"
+              style={{
+                minHeight: "10vh",
+                fontSize: "16px",
+                border: "none",
+                overflow: "auto",
+                outline: "none",
+                WebkitBoxShadow: "none",
+                MozBoxShadow: "none",
+                BoxShadow: "none",
+                resize: "none"
+              }}
+              onChange={this.checkFileContent}
+              text={this.state.fileContent}
+              options={{
+                toolbar: {
+                  buttons: ['bold', 'italic', 'underline', 'emoji']
+                },
+                buttonLabels: 'fontawesome',
+                extensions: {
+                  'emoji': this.emojiButton()
+                }
+              }}
+            />
+        }
+        <div>
+          {
+            this.state.showEmoji ? <Picker title='Pick your emoji…' emoji='point_up' set='emojione' onSelect={this.addEmoji} style={{ position: 'absolute', bottom: '20px', right: '20px' }} /> : null
+          }
+        </div>
       </Shortcuts>
     );
   }
