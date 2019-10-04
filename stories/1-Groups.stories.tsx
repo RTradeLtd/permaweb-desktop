14aebb24d456b56710dff1627847b55c850e70ad
John Kane <john@kanej.me>
2019-10-04 11:35:43 +0100

    refactor(storybook): make the storybook decorator for font-awesome global

--- stories/1-Groups.stories.tsx
+++ stories/1-Groups.stories.tsx
@@ -3,19 +3,6 @@ import { Group } from '../src/screens/Group'
 import { MemoryRouter } from 'react-router-dom'
 import NewPostEntryControl from '../src/components/NewPostEntryControl'
 
-const Frame = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
-  return (
-    <div>
-      <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css"
-        rel="stylesheet"
-        type="text/css"
-      />
-      {children}
-    </div>
-  )
-}
-
 export default {
   title: 'Groups'
 }
@@ -30,11 +17,7 @@ publicGroup.story = {
   name: 'Public Group'
 }
 
-export const newPostEnryControl = () => (
-  <Frame>
-    <NewPostEntryControl />
-  </Frame>
-)
+export const newPostEnryControl = () => <NewPostEntryControl />
 
 newPostEnryControl.story = {
   name: 'New Post Entry Control'
