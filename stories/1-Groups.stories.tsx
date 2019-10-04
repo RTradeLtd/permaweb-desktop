4f1b1b02b3aa8e1d0f6914db8374dc78217d9c36
John Kane <john@kanej.me>
2019-10-04 11:10:45 +0100

    chore: add a story for the new post entry control

--- stories/1-Groups.stories.tsx
+++ stories/1-Groups.stories.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import { Group } from '../src/screens/Group'
 import { MemoryRouter } from 'react-router-dom'
+import NewPostEntryControl from '../src/components/NewPostEntryControl'
+
+const Frame = ({ children }: { children: JSX.Element | JSX.Element[] }) => {
+  return (
+    <div>
+      <link
+        href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/css/font-awesome.min.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+      {children}
+    </div>
+  )
+}
 
 export default {
   title: 'Groups'
@@ -14,4 +28,14 @@ export const publicGroup = () => (
 
 publicGroup.story = {
   name: 'Public Group'
+}
+
+export const newPostEnryControl = () => (
+  <Frame>
+    <NewPostEntryControl />
+  </Frame>
+)
+
+newPostEnryControl.story = {
+  name: 'New Post Entry Control'
 }
