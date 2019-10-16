9f95de8021d5fafe95f5942f7f1a0b86a2537912
John Kane <john@kanej.me>
2019-10-16 14:09:34 +0100

    chore(stories): add a post story to storybook

--- stories/1-Groups.stories.tsx
+++ stories/1-Groups.stories.tsx
@@ -4,9 +4,10 @@ import { MemoryRouter } from 'react-router-dom'
 import NewPostEntryControl from '../src/components/NewPostEntryControl'
 import { action } from '@storybook/addon-actions'
 import { Post } from '../src/domain'
+import PostCard from '../src/components/PostCard'
 
 export default {
-  title: 'Groups'
+  title: 'Group'
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,24 +20,41 @@ const deletePostAction = (block: string) => {
   action('Delete Post Clicked')(block)
 }
 
-const examplePosts: Post[] = [
-  {
-    postHash: '999',
-    groupHash: '88887777766666',
-    author: 'Shokunin',
-    block: '123',
-    content: 'Hello World!',
-    lastModified: '',
-    comments: [],
-    reactions: [],
-    shares: []
+const stringToSlateContent = (text: string) => {
+  return {
+    document: {
+      nodes: [
+        {
+          object: 'block',
+          type: 'paragraph',
+          nodes: [
+            {
+              object: 'text',
+              text: text
+            }
+          ]
+        }
+      ]
+    }
   }
-]
+}
+
+const exampleShortPost: Post = {
+  postHash: '999',
+  groupHash: '88887777766666',
+  author: 'Shokunin',
+  block: '123',
+  content: stringToSlateContent('Hello World!'),
+  lastModified: '2019-01-01T10:00:00Z',
+  comments: [],
+  reactions: [],
+  shares: []
+}
 
 export const publicGroup = () => (
   <MemoryRouter>
     <Group
-      posts={examplePosts}
+      posts={[exampleShortPost]}
       onPostClicked={addPostAction}
       onPostDelete={deletePostAction}
     />
@@ -54,3 +72,9 @@ export const newPostEnryControl = () => (
 newPostEnryControl.story = {
   name: 'New Post Entry Control'
 }
+
+export const shortPost = () => (
+  <MemoryRouter>
+    <PostCard onPostDelete={deletePostAction} post={exampleShortPost} />
+  </MemoryRouter>
+)
